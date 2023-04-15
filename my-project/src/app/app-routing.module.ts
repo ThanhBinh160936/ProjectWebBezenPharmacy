5253d3b300c5b79f6f104236f5486b3725d964e3
nganphan2001 <119951398+nganphan2001@users.noreply.github.com>
2023-04-15 15:13:44 +0700

    update infor

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InforComponent } from './infor/infor.component';
+import { PolicydeliveryComponent } from './infor/policydelivery/policydelivery.component';
+import { PolicypaymentComponent } from './infor/policypayment/policypayment.component';
+import { PolicysecurityComponent } from './infor/policysecurity/policysecurity.component';
+import { PolicywarrantyComponent } from './infor/policywarranty/policywarranty.component';
+import { MemberpointComponent } from './infor/memberpoint/memberpoint.component';
+import { OverviewComponent } from './infor/overview/overview.component';
+import { InstructionComponent } from './infor/instruction/instruction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'app-overview',component:OverviewComponent},
+  {path:'app-instruction',component:InstructionComponent},
+  {path:'app-memberpoint',component:MemberpointComponent},
+  {path:'app-policydelivery',component:PolicydeliveryComponent},
+  {path:'app-policypayment',component:PolicypaymentComponent},
+  {path:'app-policysecurity',component:PolicysecurityComponent},
+  {path:'app-policywarranty',component:PolicywarrantyComponent},
+
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -9,5 +26,13 @@ const routes: Routes = [];
 })
 export class AppRoutingModule { }
 export const RoutingComponent=[
+  InforComponent,
+  PolicydeliveryComponent,
+  PolicypaymentComponent,
+  PolicysecurityComponent,
+  PolicywarrantyComponent,
+  MemberpointComponent,
+  OverviewComponent,
+  InstructionComponent
   
 ]
