5bf0f856fc2c0c4ce5c1ec85c9fb322a83621c09
Nguyễn Thành Danh <nguyenthanhdanh27022002@gmail.com>
2023-04-15 21:24:50 +0700

    Này để test thoi

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path: "shopping-cart", component: ShoppingcartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -9,5 +15,5 @@ const routes: Routes = [];
 })
 export class AppRoutingModule { }
 export const RoutingComponent=[
-  
+  ShoppingcartComponent
 ]
