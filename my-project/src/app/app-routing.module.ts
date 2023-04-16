d7ad6417a1ebe28f03c128dba9b5d42a44b54f34
LuongThu186 <thula20406@st.uel.edu.vn>
2023-04-16 21:03:13 +0700

    create login

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
