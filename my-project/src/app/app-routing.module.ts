4e5d7be8d1e0c5f5dff07d5a5e8a3a85e01b1819
Nguyễn Thành Danh <nguyenthanhdanh27022002@gmail.com>
2023-04-17 09:13:56 +0700

    Danh update shopping-cart

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"shopping-cart", component:ShoppingcartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
