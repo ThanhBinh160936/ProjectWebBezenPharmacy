ee2bff21c01d0b1101d981dd1f285fdc8d7390a6
Nguyễn Thành Danh <nguyenthanhdanh27022002@gmail.com>
2023-04-17 20:06:55 +0700

    Mot so thay doi cua Danh

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { PaymentBankingComponent } from './payment-banking/payment-banking.component';
+import { PaymentMomoComponent } from './payment-momo/payment-momo.component';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
-  {path:"shopping-cart", component:ShoppingcartComponent}
+  {path:"shopping-cart", component:ShoppingcartComponent},
+  {path:"payment-banking", component:PaymentBankingComponent},
+  {path:"payment-momo", component:PaymentMomoComponent},
+  {path:"payment-member", component:PaymentComponent}
 ];
 
 @NgModule({
